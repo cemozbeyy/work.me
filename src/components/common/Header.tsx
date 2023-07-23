3ee8c0d4be8246f4859df638055c4791a0956778
cemozbey <48527545+cemozbeyy@users.noreply.github.com>
2023-07-23 18:45:11 +0300

    header in procces

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.scss";
+import logout from "../../assets/logout.svg";
+import logo from "../../assets/logo.jpeg";
+
 const Header: React.FC<{}> = () => {
-  // Doğru kullanım: Bir ReactNode döndürüyor
-  return <div className="ss">Header!</div>;
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+  return (
+    <header className="header-container">
+      <section className="left-side">
+        <div className="logo">W | Work.me</div>
+        <div className="spacing"></div>
+        <div className={`input-container ${isFocused ? "focused" : ""}`}>
+          <input
+            type="text"
+            placeholder="Search"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          />
+        </div>
+      </section>
+      <div className="profile">
+        {/* Profil ikonları ve çıkış yap butonu buraya eklenebilir */}
+        <div className="profile-icon"></div>
+        <button className="logout-button">
+          <img width={20} height={20} src={logout} alt="Logo" />
+        </button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
