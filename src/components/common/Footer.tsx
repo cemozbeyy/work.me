5161d1a774cd4ff6c197d83160057543eaf77e5f
cemozbey <48527545+cemozbeyy@users.noreply.github.com>
2023-07-25 01:08:55 +0300

    warrning fixed

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -7,30 +7,47 @@ const Footer: React.FC<{}> = () => {
     <div className="footer">
       <ul>
         <li>
-          <a target="_blank" href="https://twitter.com/__cmew__">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/__cmew__"
+          >
             Twitter
           </a>
         </li>
         <li>
-          <a target="_blank" href="https://codepen.io/oozbey">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://codepen.io/oozbey"
+          >
             Codepen
           </a>
         </li>
         <li>
-          <a target="_blank" href="mailto:cemozbey8@gmail.com">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="mailto:cemozbey8@gmail.com"
+          >
             Email
           </a>
         </li>
         <li>
           <a
             target="_blank"
+            rel="noopener noreferrer"
             href="https://www.linkedin.com/in/cem-%C3%B6zbey-321827207"
           >
             LinkedIn
           </a>
         </li>
         <li>
-          <a target="_blank" href="https://github.com/cemozbeyy">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/cemozbeyy"
+          >
             Github
           </a>
         </li>
